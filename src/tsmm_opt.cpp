b7a6e5d867f9076fe7ebc76d1af118236ead17df
muuuchen <745754920@qq.com>
2025-05-29 14:14:48 +0000

    upd

--- src/tsmm_opt.cpp
+++ src/tsmm_opt.cpp
@@ -1,17 +1,19 @@
-#include "../include/final.h"
 #include "../include/micro_kernel24x8.h"
 #include "timer.h" // 包含 Timer 类头文件
 #include <iostream>
+
 int main() {
-  const int M = 24 * 100;
-  const int N = 8 * 100;
+  const int M = 24 * 1000;
+  const int N = 8 * 1000;
   const int K = 128;
   double A[M * K];
   double B[K * N];
   double C[M * N];
   Timer timer("tsmm_kernel", M, N, K);
   timer.start();
-  mydgemm_cpu_v18(M, N, K, 1.0, A, M, B, K, 0, C, M);
+  kernel_tsmm<24, 8>(A, B, C, M, N, K, M, K, M);
+  //   mydgemm_cpu_v18(M, N, K, 1.0, A, M, B, K, 0, C, M);
+
   timer.stop();
 
   timer.print_result();
