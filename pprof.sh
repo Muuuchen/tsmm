179685d006156810fa19ddfeb5071ffec5c29d72
Muuuchen <745754920@qq.com>
2025-06-01 12:54:30 +0000

    upd

--- pprof.sh
+++ pprof.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # CPUPROFILE=prof.out ./build/tsmm 
-pprof --text ./build/tsmm prof.out
+pprof --text ./build/bench_blas prof.out
 # # 或者生成可视化报告
 # pprof --web ./build/tsmm prof.out
--- src/tsmm_opt.cpp
+++ src/tsmm_opt.cpp
@@ -7,26 +7,33 @@
 const int64_t ALIGNMENT = 64;
 #define ALIGN(ptr) (double *)(((int64_t)(ptr) + ALIGNMENT) & (~(ALIGNMENT - 1)))
 int main() {
-  const int M = 24 * 2000;
-  const int N = 8 * 1000;
-  const int K = 128;
-  alignas(64) double A[M * K];
-  alignas(64) double B[K * N];
-  alignas(64) double C[M * N];
-  Timer timer("tsmm_kernel", M, N, K);
-  timer.start();
-  /* kernel_tsmm<16, 2>(A, B, C, M, N, K, M, K, M);
-   mydgemm_cpu_v18(M, N, K, 1.0, A, M, B, K, 0, C, M);*/
-  tsmm_block_pack(A, B, C, M, N, K, M, K, M);
-  timer.stop();
-  timer.print_result();
-  timer.set_name("tsmm_prefetch");
-  double *A_prefetch = (double *)malloc(64 + M * (K + 1) * sizeof(double));
+  const int problem_sizes[4][3] = {
+      {40000, 16000, 128}, // 第1组
+      {8, 16, 16000},      // 第2组
+      {32, 16000, 16},     // 第3组
+      {144, 144, 144}      // 第4组
+  };
+  for (auto problem_size : problem_sizes) {
+    const int M = problem_size[0];
+    const int N = problem_size[1];
+    const int K = problem_size[2];
+    alignas(64) double A[M * K];
+    alignas(64) double B[K * N];
+    alignas(64) double C[M * N];
+    Timer timer("tsmm_kernel", M, N, K);
+    timer.start();
+    /* kernel_tsmm<16, 2>(A, B, C, M, N, K, M, K, M);
+     mydgemm_cpu_v18(M, N, K, 1.0, A, M, B, K, 0, C, M);*/
+    tsmm_block_pack(A, B, C, M, N, K, M, K, M);
+    timer.stop();
+    timer.print_result();
+    timer.set_name("tsmm_prefetch");
+    double *A_prefetch = (double *)malloc(64 + M * (K + 1) * sizeof(double));
 
-  timer.start();
-  tsmm_prefetch(M, N, K, ALIGN(A_prefetch), M, ALIGN(B), K, ALIGN(C), M);
-  timer.stop();
-  timer.print_result();
-
+    timer.start();
+    tsmm_prefetch(M, N, K, ALIGN(A_prefetch), M, ALIGN(B), K, ALIGN(C), M);
+    timer.stop();
+    timer.print_result();
+  }
   return 0;
 }
